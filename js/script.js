2d8dfe3f54b5846f03aef72312541e1f3bc9579f
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2021-12-28 10:39:03 -0600

    question is now appearing on broswer

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ var questions = document.querySelector("#questions");
 var answerA = document.querySelector("#answerA");
 var answerB = document.querySelector("#answerB");
 var answerC = document.querySelector("#answerC");
-var answerD= document.querySelector("#answerD");
+var answerD = document.querySelector("#answerD");
 var answer = document.querySelector("#answer");
 var feedback = document.querySelector("#feedback");
 var multipleAnswers = document.querySelector("#multipleAnswers");
@@ -31,63 +31,95 @@ var timeInt;
 //__________________
 //Running Timer
 function timer() {
-    timeInt= setInterval(function() {
-        timeLengthLeft--;
-        timerClock.textContent = " Time Left: " + timeLengthLeft;
-        
-        if (timeLengthLeft === 0 || qb >= quizQuestions.length) {
-          clearInterval(timeInt);
-          gameOver();
-        }
-    }, 1000);
+  timeInt = setInterval(function () {
+    timeLengthLeft--;
+    timerClock.textContent = " Time Left: " + timeLengthLeft;
 
-    
+    if (timeLengthLeft === 0 || qb >= quizQuestions.length) {
+      clearInterval(timeInt);
+      gameOver();
+    }
+  }, 1000);
 }
-// break- function to run through questions 
+// break- function to run through questions
 function showQuestions() {
   if (qb < quizQuestions.length) {
-    questions.textContent = quizQuestions[qb].questions; 
-    answerA.textContent = quizQuestions[qb].choices[0]; 
-    answerB.textContent = quizQuestions[qb].choices[1]; 
-    answerC.textContent = quizQuestions[qb].choices[2]; 
-    answerD.textContent = quizQuestions[qb].choices[3]; 
+    questions.textContent = quizQuestions[qb].questions;
+    answerA.textContent = quizQuestions[qb].choices[0];
+    answerB.textContent = quizQuestions[qb].choices[1];
+    answerC.textContent = quizQuestions[qb].choices[2];
+    answerD.textContent = quizQuestions[qb].choices[3];
   } else {
-    gameOver(); 
+    gameOver();
   }
 }
-//checking to make sure answer is right or wrong 
+//checking to make sure answer is right or wrong
 function validateAnswer(event) {
   if (qb >= quizQuestions.length) {
-
-    gameOver(); 
+    gameOver();
     clearInterval(timeInt);
   } else {
     if (event === quizQuestions[qb].answer) {
-      feedback.textContent = "Correct Great Job!"; 
+      feedback.textContent = "Correct Great Job!";
     } else {
-      timeLengthLeft -= 10; 
-      feedback.textContent = "Sorry! That is wrong!"
+      timeLengthLeft -= 10;
+      feedback.textContent = "Sorry! That is wrong!";
     }
-    score = timeLengthLeft; 
-    qb++; 
-    showQuestions(); 
+    score = timeLengthLeft;
+    qb++;
+    showQuestions();
   }
 }
-// function gameOver() { 
-//   scoreButton.innerHTML = score; 
-//   scoreButton.style.display = "inline-block"; 
-//   gameCard.classList.add("hide"); 
-//   inputForm.classList.remove("hide"); 
-//   timerClock.classList.add("hide"); 
-//   highScore.classList.add("hide"); 
-  
-// }
+function gameOver() {
+  scoreButton.innerHTML = score;
+  scoreButton.style.display = "inline-block";
+  gameCard.classList.add("hide");
+  inputForm.classList.remove("hide");
+  timerClock.classList.add("hide");
+  highScore.classList.add("hide");
+  leaderScores();
+}
+function leaderScores() {
+  removeFromLeaderScores();
+  addToLeaderScores();
+  scoreList.sort((a, b) => {
+    return b.score - a.score;
+  });
 
+  //renders the top 7 highest scores
+  topSeven = scoreList.slice(0, 7);
 
-// adding begin Quiz eventhandlers
+  for (var i = 0; i < topSeven.length; i++) {
+    var gamer = topSeven[i].gamer; 
+    var score = topSeven[i].score; 
+    
+    var newDiv = document.createElement("div"); 
+    leaderScoresDiv.appendChild(newDiv); 
+
+    var newName = document.createElement("label")
+    newName.textContent = gamer + " - " + score; 
+    newDiv.appendChild(newName); 
+  }
+}
+//adds top games to Leaderboard
+function addToLeaderBoard() {
+  highScoreDiv = document.createElement("div"); 
+  highScoreDiv.setAttribute("id", "gamerInitials"); 
+  document.getElementById("highScore")
+}
+
+function removeFromLeaderScores() {
+  var removeLowerScores = document.getElementById("gamerInitials");
+  if (removeLowerScores != null) {
+    removeLowerScores.remove();
+  } else {
+
+  }
+}
+// adding begin Quiz event handlers
 
 startQuiz.addEventListener("click", function (event) {
-timer();
+  timer();
   showQuestions();
   begin.classList.add("hide");
   gameCard.classList.remove("hide");
@@ -98,4 +130,3 @@ gameCard.addEventListener("click", function (event) {
   var event = event.target;
   validateAnswer(event.textContent.trim());
 });
-
