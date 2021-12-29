4cbea3ff43a3ed18c05ee200498569f486f83215
Crystal Soto-Estrada <Csoto.lacombe@gmail.com>
2021-12-29 11:33:56 -0600

    quiz buttons are all  functioning properly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Global variables, questions are stored in questions.js
 var startQuiz = document.querySelector("#startButton");
-var highScore = document.querySelector("#highScoreBtn");
+var highScoreBtn = document.querySelector("#highScoreBtn");
 var timerClock = document.querySelector(".timer");
 var gameCard = document.querySelector("#gameCard");
 var questions = document.querySelector("#questions");
@@ -12,7 +12,7 @@ var answer = document.querySelector("#answer");
 var feedback = document.querySelector("#feedback");
 var multipleAnswers = document.querySelector("#multipleAnswers");
 var inputForm = document.querySelector("#inputForm");
-var scoreCard = document.querySelector("#scoreCard");
+var tallyCard = document.querySelector("#tallyCard");
 var scoreButton = document.querySelector("#scoreButton");
 var initials = document.querySelector("#initials");
 var submitButton = document.querySelector("#submitButton");
@@ -24,10 +24,10 @@ var timeLengthLeft = quizQuestions.length * 15;
 var qb = 0;
 var sb = 0;
 var score = 0;
-var scoreList = [];
+var scorePool= [];
 var timeInt;
 
-//getTotalScore();
+getTotalScore();
 //__________________
 //Running Timer
 function timer() {
@@ -70,24 +70,35 @@ function validateAnswer(event) {
     showQuestions();
   }
 }
+//moving on to saving and updating 
+function getTotalScore() {
+  var storedScore = JSON.parse(localStorage.getItem("highestScore"));
+  if (storedScore != null) {
+    scorePool = storedScore; 
+  }
+}
+function savePoints() {
+  localStorage.setItem("highestScore", JSON.stringify(scorePool)); 
+}
+////////////////////////////////////////////////////
 function gameOver() {
   scoreButton.innerHTML = score;
   scoreButton.style.display = "inline-block";
   gameCard.classList.add("hide");
   inputForm.classList.remove("hide");
   timerClock.classList.add("hide");
-  highScore.classList.add("hide");
+  highScoreBtn.classList.add("hide");
   leaderScores();
 }
 function leaderScores() {
   removeFromLeaderScores();
   addToLeaderScores();
-  scoreList.sort((a, b) => {
+  scorePool.sort((a, b) => {
     return b.score - a.score;
   });
 
   //renders the top 7 highest scores
-  topSeven = scoreList.slice(0, 7);
+  topSeven = scorePool.slice(0, 7);
 
   for (var i = 0; i < topSeven.length; i++) {
     var gamer = topSeven[i].gamer; 
@@ -101,11 +112,11 @@ function leaderScores() {
     newDiv.appendChild(newName); 
   }
 }
-//adds top games to Leaderboard
-function addToLeaderBoard() {
-  highScoreDiv = document.createElement("div"); 
-  highScoreDiv.setAttribute("id", "gamerInitials"); 
-  document.getElementById("highScore")
+//adds top gamers to Leaderboard
+function addToLeaderScores() {
+  leaderScoresDiv = document.createElement("div");
+  leaderScoresDiv.setAttribute("id", "gamerInitials");
+  document.getElementById("leaderScores").appendChild(leaderScoresDiv);
 }
 
 function removeFromLeaderScores() {
@@ -116,17 +127,55 @@ function removeFromLeaderScores() {
 
   }
 }
-// adding begin Quiz event handlers
+// adding event handlers
+//Begin Quiz handler 
 
 startQuiz.addEventListener("click", function (event) {
   timer();
   showQuestions();
   begin.classList.add("hide");
   gameCard.classList.remove("hide");
-  highScore.style.display = "none";
-  scoreCard.classList.add("hide");
+  highScoreBtn.style.display = "none";
+  tallyCard.classList.add("hide");
 });
-gameCard.addEventListener("click", function (event) {
+multipleAnswers.addEventListener("click", function (event) {
   var event = event.target;
   validateAnswer(event.textContent.trim());
 });
+
+  submitButton.addEventListener("click", function (event) {
+    //event.preventDefault();
+    var gamerInitials = initials.value.trim();
+    var updatedScore = {
+      gamer: gamerInitials,
+      score: score,
+    };
+
+    scorePool.push(updatedScore);
+    savePoints();
+    leaderScores();
+    inputForm.classList.add("hide");
+    tallyCard.classList.add("hide");
+  });
+
+highScoreBtn.addEventListener("click", function (event) {
+  tallyCard.classList.remove("hide");
+  highScoreBtn.classList.add("hide");
+  begin.classList.add("hide");
+  leaderScores();
+}); 
+
+//Event listener for back btn 
+
+backButton.addEventListener("click", function (event) {
+  location.reload();
+}); 
+
+//clear button event listens 
+clearButton.addEventListener("click", function (event) {
+  scorePool = [];
+  begin.classList.add("hide");
+  localStorage.setItem("highestScore", JSON.stringify(scorePool));
+  leaderScores();
+  savePoints();
+}); 
